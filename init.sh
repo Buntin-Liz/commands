8a252a6e3162bfe66d370ec501bee7396710145c
Buntintin <108047451+Buntin-Liz@users.noreply.github.com>
2023-08-07 08:20:42 +0000

    init.sh -> TODO:checkpath(or path)

--- init.sh
+++ init.sh
@@ -1,4 +1,30 @@
+#!/usr/bin/env sh
 
+CURRENT_DIR=$(cd $(dirname $0); pwd)
+
+if [ -e "init.sh" ]; then
+    echo "dir is healthful."
+else
+    echo "run in commands dir"
+    exit 1
+fi
+
+
+SCRIPT_DIR=$CURRENT_DIR/scripts
+if [ ! -d "scripts" ]; then
+    mkdir scripts
+fi
+
+cd scripts
+
+# import testssl.sh
+if [ ! -d "testssl" ]; then
+    mkdir testssl
+fi
+cd testssl
 wget https://testssl.sh/testssl.sh
 chmod +x testssl.sh
+cd $CURRENT_DIR
+ln -s $SCRIPT_DIR/testssl/testssl.sh testssl
 
+#TODO:path通し自動化
