930c90d5341110bfa0305a04f3500f78efd5eacc
Buntin-Synthia <aokitakumi72@gmail.com>
2023-08-30 16:17:00 +0900

    [sshls.ts]-a(showAll)オプション追加。

--- src/js/sshls.ts
+++ src/js/sshls.ts
@@ -4,135 +4,137 @@ import { promises as fs } from 'fs';
 import os from 'os';
 
 type Host = {
-    alias: string[];
-    hostname: string;
-    user: string;
-    port: number;
-    identityfile: string;
-    password?: string;
-    keys: string[],
-    contents: string[]
+  alias: string[];
+  hostname: string;
+  user: string;
+  port: number;
+  identityfile: string;
+  password?: string;
+  keys: string[],
+  contents: string[]
 }
 
 const parseHost = (hostsection: string): Host => {
-    const lines = hostsection.split('\n');
-    //if line has "Host " then host is alias
-    let host: Host = {
-        alias: [],
-        hostname: '',
-        user: '',
-        port: 22,
-        identityfile: '',
-        password: undefined,
-        keys: [],
-        contents: [],
-    };
-    lines.map((line) => line.trim()).forEach((line) => {
-        if (line.startsWith('Host ')) {
-            host.alias = line.replace('Host ', '').split(' ');
-        }
-        if (line.startsWith('HostName ')) {
-            host.hostname = line.replace('HostName ', '').trim();
-        }
-        if (line.startsWith('Hostname ')) {
-            host.hostname = line.replace('Hostname ', '').trim();
-        }
-        if (line.startsWith('User ')) {
-            host.user = line.replace('User ', '').trim();
-        }
-        if (line.startsWith('Port ')) {
-            host.port = parseInt(line.replace('Port ', ''));
-        }
-        if (line.startsWith('IdentityFile ')) {
-            host.identityfile = line.replace('IdentityFile ', '').trim();
-        }
-        //parse options
-        if (line.startsWith('#')) {
-            const key_value_pair = line.replace('#', '');
-            const [key, value] = key_value_pair.split(' ');
-            host.keys.push(key);
-            host.contents.push(value);
-        }
-    });
-    return host;
+  const lines = hostsection.split('\n');
+  //if line has "Host " then host is alias
+  let host: Host = {
+    alias: [],
+    hostname: '',
+    user: '',
+    port: 22,
+    identityfile: '',
+    password: undefined,
+    keys: [],
+    contents: [],
+  };
+  lines.map((line) => line.trim()).forEach((line) => {
+    if (line.startsWith('Host ')) {
+      host.alias = line.replace('Host ', '').split(' ');
+    }
+    if (line.startsWith('HostName ')) {
+      host.hostname = line.replace('HostName ', '').trim();
+    }
+    if (line.startsWith('Hostname ')) {
+      host.hostname = line.replace('Hostname ', '').trim();
+    }
+    if (line.startsWith('User ')) {
+      host.user = line.replace('User ', '').trim();
+    }
+    if (line.startsWith('Port ')) {
+      host.port = parseInt(line.replace('Port ', ''));
+    }
+    if (line.startsWith('IdentityFile ')) {
+      host.identityfile = line.replace('IdentityFile ', '').trim();
+    }
+    //parse options
+    if (line.startsWith('#')) {
+      const key_value_pair = line.replace('#', '');
+      const [key, value] = key_value_pair.split(' ');
+      host.keys.push(key);
+      host.contents.push(value);
+    }
+  });
+  return host;
 }
 
 const splitByHosts = (data: string): string[] => {
-    const lines = data.split('\n');
-    const hostSections: string[] = [];
-    let currentSection = '';
-    for (const line of lines) {
-        if (line.startsWith('Host ')) {
-            if (currentSection.trim() !== '') {
-                hostSections.push(currentSection.trim());
-                currentSection = '';
-            }
-        }
-        currentSection += line + '\n';
-    }
-    if (currentSection.trim() !== '') {
+  const lines = data.split('\n');
+  const hostSections: string[] = [];
+  let currentSection = '';
+  for (const line of lines) {
+    if (line.startsWith('Host ')) {
+      if (currentSection.trim() !== '') {
         hostSections.push(currentSection.trim());
+        currentSection = '';
+      }
     }
-    return hostSections;
+    currentSection += line + '\n';
+  }
+  if (currentSection.trim() !== '') {
+    hostSections.push(currentSection.trim());
+  }
+  return hostSections;
 }
 
 const calculateMaxLengths = (hosts: Host[]) => {
-    let maxAliasLength = 0;
-    let maxHostnameLength = 0;
-    let maxPortLength = 0;
-    let maxUserLength = 0;
-    hosts.forEach(host => {
-        if (JSON.stringify(host.alias).length > maxAliasLength) {
-            maxAliasLength = JSON.stringify(host.alias).length;
-        }
-        if (host.hostname.length > maxHostnameLength) {
-            maxHostnameLength = host.hostname.length;
-        }
-        if (host.port.toString().length > maxPortLength) {
-            maxPortLength = host.port.toString().length;
-        }
-        if (host.user.length > maxUserLength) {
-            maxUserLength = host.user.length;
-        }
-    });
-    return {
-        maxAliasLength,
-        maxHostnameLength,
-        maxPortLength,
-        maxUserLength
-    };
+  let maxAliasLength = 0;
+  let maxHostnameLength = 0;
+  let maxPortLength = 0;
+  let maxUserLength = 0;
+  hosts.forEach(host => {
+    if (JSON.stringify(host.alias).length > maxAliasLength) {
+      maxAliasLength = JSON.stringify(host.alias).length;
+    }
+    if (host.hostname.length > maxHostnameLength) {
+      maxHostnameLength = host.hostname.length;
+    }
+    if (host.port.toString().length > maxPortLength) {
+      maxPortLength = host.port.toString().length;
+    }
+    if (host.user.length > maxUserLength) {
+      maxUserLength = host.user.length;
+    }
+  });
+  return {
+    maxAliasLength,
+    maxHostnameLength,
+    maxPortLength,
+    maxUserLength
+  };
+}
+
+const printHostInfo = (host: Host, args: string[], lengths: any) => {
+  const showAll = args.includes('-a');
+  const showPort = args.includes('-p') || showAll;
+  const showUser = args.includes('-u') || showAll;
+  const showOps = args.includes('-o') || showAll;
+  let opdata = '';
+  if (host.keys.length !== 0) {
+    opdata = host.keys.map((key, index) => `${ key }:${ host.contents[index] }`).join('|')
+  }
+  const aliasSecondary = host.alias.length === 1 ? host.alias[0] : JSON.stringify(host.alias);
+  const aliasStr = aliasSecondary.padEnd(lengths.maxAliasLength);
+  const hostnameStr = host.hostname.padEnd(lengths.maxHostnameLength);
+  const portStr = showPort ? `:${ host.port.toString().padEnd(lengths.maxPortLength) }` : '';
+  const userStr = showUser ? host.user.padEnd(lengths.maxUserLength) : '';
+  const opsStr = showOps ? opdata : '';
+  echo(`${ aliasStr } ${ hostnameStr }${ portStr } ${ userStr }${ opsStr }`);
 }
 
 
 
 (async () => {
-    const args = process.argv;
-    const showPort = args.includes('-p');
-    const showUser = args.includes('-u');
-    const showOps = args.includes('-o');
-    try {
-        const home = os.homedir();
-        const data = await fs.readFile(`${home}/.ssh/config`, 'utf8');
-        const hostSections = splitByHosts(data);
-        const hosts = hostSections.map(parseHost);
-        const lengths = calculateMaxLengths(hosts);
-
-        hosts.forEach(host => {
-            let opdata = '';
-            if (host.keys.length !== 0) {
-                opdata = host.keys.map((key, index) => `${key}:${host.contents[index]}`).join('|')
-            }
-            const aliasSecondary = host.alias.length === 1 ? host.alias[0] : JSON.stringify(host.alias);
-            const aliasStr = aliasSecondary.padEnd(lengths.maxAliasLength);
-            const hostnameStr = host.hostname.padEnd(lengths.maxHostnameLength);
-            const portStr = showPort ? `:${host.port.toString().padEnd(lengths.maxPortLength)}` : '';
-            const userStr = showUser ? host.user.padEnd(lengths.maxUserLength) : '';
-            const opsStr = showOps ? opdata : '';
-            echo(`${aliasStr} ${hostnameStr}${portStr} ${userStr}${opsStr}`);
-        });
-    } catch (error) {
-        console.error(`Error reading or parsing SSH config: ${(error as any).message}`);
-    }
+  const args = process.argv;
+  try {
+    const home = os.homedir();
+    const data = await fs.readFile(`${ home }/.ssh/config`, 'utf8');
+    const hostSections = splitByHosts(data);
+    const hosts = hostSections.map(parseHost);
+    const lengths = calculateMaxLengths(hosts);
+    hosts.forEach(host => printHostInfo(host, args, lengths));
+  } catch (error) {
+    console.error(`Error reading or parsing SSH config: ${ (error as any).message }`);
+  }
 })();
 
 
