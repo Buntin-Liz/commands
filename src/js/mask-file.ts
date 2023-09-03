7baa1944ee71c0c4fd045fd4e3028457cd0a7e7d
Buntin-LArchel <mail@buntin.xyz>
2023-09-03 17:44:16 +0900

    a

--- src/js/mask-file.ts
+++ src/js/mask-file.ts
@@ -4,10 +4,12 @@ import "zx/globals";
 $.verbose = false;
 
 const commandName = "mask-file";
+argv._ = argv._.filter((t) => !t.includes("commands/" + commandName));
+
 
 await (async () => {
   //filter the commandName from argv._
-  const targets = argv._.filter((t) => !t.includes("commands/" + commandName));
+  const targets = argv._;
   const pwdp = await $`pwd`;
 
   const histories = await Promise.all(
