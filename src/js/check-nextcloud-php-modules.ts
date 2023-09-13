dfba654102cf8fd23ecf87e40f4cb16c192ecdce
Buntin-Rhea <mail@buntin.xyz>
2023-09-13 11:04:53 +0900

    多分一回しか使わないスクリプト

---
+++ src/js/check-nextcloud-php-modules.ts
@@ -0,0 +1,44 @@
+#!/usr/bin/env zx
+
+
+const isModuleInstalled = async (moduleName: string) => {
+  try {
+    const { stdout } = await $`php -m | grep -i ${moduleName}`;
+    return !!stdout;
+  } catch (e) {
+    return false;
+  }
+}
+
+const phpModules = [
+  { name: "ctype", pkgName: "php-ctype" },
+  { name: "curl", pkgName: "php-curl" },
+  { name: "dom", pkgName: "php-dom" },
+  { name: "fileinfo", pkgName: "php-fileinfo" },
+  { name: "filter", pkgName: "php-filter" },
+  { name: "GD", pkgName: "php-gd" },
+  { name: "hash", pkgName: "php-hash" },
+  { name: "JSON", pkgName: "php-json" },
+  { name: "libxml", pkgName: "php-libxml" },
+  { name: "mbstring", pkgName: "php-mbstring" },
+  { name: "openssl", pkgName: "php-openssl" },
+  { name: "posix", pkgName: "php-posix" },
+  { name: "session", pkgName: "php-session" },
+  { name: "SimpleXML", pkgName: "php-simplexml" },
+  { name: "XMLReader", pkgName: "php-xmlreader" },
+  { name: "XMLWriter", pkgName: "php-xmlwriter" },
+  { name: "zip", pkgName: "php-pecl-zip" },
+  { name: "zlib", pkgName: "php-zlib" }
+];
+
+(async () => {
+  for (const module of phpModules) {
+    if (!(await isModuleInstalled(module.name))) {
+      console.log(`${module.name} モジュールが見つかりません。インストールします。`);
+      await $`sudo yum install -y ${module.pkgName}`;
+    } else {
+      console.log(`${module.name} モジュールはインストール済みです。`);
+    }
+  }
+  echo("PHP モジュールのインストールが完了しました。");
+})();
