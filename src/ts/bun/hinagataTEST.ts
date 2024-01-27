66a4a2e1c718b35df7a5d7f84db93e7d1d10e225
Buntin-Synthia <mail@buntin.xyz>
2024-01-28 01:26:51 +0900

    Macから,Bun Shell-.TypeScript対応につき本格書き換え

--- src/ts/bun/hinagataTEST.ts
+++ src/ts/bun/hinagataTEST.ts
@@ -1,13 +1,16 @@
 #!/usr/bin/env bun
-import { $ } from 'zx';
-import "zx/globals";
-
-const args = argv;
+import { $ } from 'bun';
+import { parseArgs } from 'util';
+const args = parseArgs({
+  args: Bun.argv,
+  options: {},
+  strict: true,
+  allowPositionals: true,
+});
 
 (async () => {
   // 引数に基づいて何かする（例）
   console.log(`引数: `, args);
 
   const result = await $`echo "Hello from zx in Bun!"`;
-  console.log(JSON.stringify(result, null, 2));
 })();
