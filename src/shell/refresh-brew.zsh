2092442c537a1f6c9a53a52d5ef9a48aa83d80fa
Buntin-Synthia <mail@buntin.xyz>
2024-02-06 20:54:01 +0900

    edit refresh-brew.zsh a little

--- src/shell/refresh-brew.zsh
+++ src/shell/refresh-brew.zsh
@@ -1,6 +1,10 @@
 #!/bin/zsh
-
+echo "Refreshing Homebrew"
+echo "Updating Homebrew"
 brew update
+echo "Upgrading Homebrew"
 brew upgrade
+echo "Cleaning up Homebrew"
 brew cleanup
+echo "Removing old versions"
 brew autoremove
