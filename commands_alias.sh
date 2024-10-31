225dfa018762844cbeecc0fea93acfa759a7c2b2
Buntin-Synthia <mail@buntin.xyz>
2024-10-31 21:18:09 +0900

    add: add alias tree options

--- commands_alias.sh
+++ commands_alias.sh
@@ -3,3 +3,6 @@
 # https://github.com/badcompany-tokyo/sshls
 alias sshls='deno run -RE https://raw.githubusercontent.com/badcompany-tokyo/sshls/refs/heads/main/src/main.ts'
 
+# tree
+alias tree='tree -a -I "\.DS_Store|\.git|node_modules|vendor\/bundle" -N'
+
